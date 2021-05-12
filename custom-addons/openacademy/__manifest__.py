1ff6081ebcce3853e25bfb098318e285bc8e52fd
lam268 <lamlevu26@gmail.com>
2021-05-12 11:04:36 +0700

    update basic views

--- custom-addons/openacademy/__manifest__.py
+++ custom-addons/openacademy/__manifest__.py
@@ -25,6 +25,7 @@
     # always loaded
     'data': [
         # 'security/ir.model.access.csv',
+        'views/menu_courses.xml',
         'views/views.xml',
         'views/templates.xml',
         'views/course_views.xml',
